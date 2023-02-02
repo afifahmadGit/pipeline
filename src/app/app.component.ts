1bb85ecccf3fa4a47fc84e68b7824571891057ce
Afifahmad <afifahmadn87@gmail.com>
2023-02-02 14:16:17 +0530

    sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
+// import { registerLocaleData } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { OnedriveServiceService } from './services/onedrive-service.service';
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test';
+  folders: Section[] = [];
+  list:Section[]=[]
+  listContent:any
+  listHead: any;
+  slice: any;
+
+  constructor(private onedriver: OnedriveServiceService,private _formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+  }
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {MatStepperModule} from '@angular/material/stepper';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatStepperModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
